01f9c1a598024dbc0271555365633cb118539717
william_brooks_miller <brooksmiller.miller@gmail.com>
2020-02-09 18:10:34 -0500

    Implement Board#one_side_empty?

--- lib/board.rb
+++ lib/board.rb
@@ -48,7 +48,17 @@ class Board
   end
 
   def next_turn(ending_cup_idx)
-    # helper method to determine whether #make_move returns :switch, :prompt, or ending_cup_idx
+    # helper method to determine what #make_move returns
+    if ending_cup_idx == 6 || ending_cup_idx == 13
+      # ended on store -- get to choose where to start again
+      :prompt
+    elsif @cups[ending_cup_idx].count == 1
+      # ended on empty cup -- switches players' turns
+      :switch
+    else
+      # ended on cup with stones in it -- automatically starts there
+      ending_cup_idx
+    end
   end
 
   def render
@@ -60,6 +70,10 @@ class Board
   end
 
   def one_side_empty?
+    side1 = @cups[0...6]
+    side2 = @cups[7...13]
+    return true if side1.all? {|cup| cup.empty?} || side2.all?{|cup| cup.empty?}
+    return false
   end
 
   def winner
